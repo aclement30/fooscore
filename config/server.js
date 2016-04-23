063f21f328b83619b9a6c19f98d4ce457a7c94a7
Alexandre Clément <web@alexandreclement.com>
2016-04-23 17:28:11 -0400

    Fix typo in env vars

--- config/server.js
+++ config/server.js
@@ -8,7 +8,7 @@ var server = {
     googleOAuth: {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URl
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
 
     sessionSecretKey: process.env.SESSION_KEY,
